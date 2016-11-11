866e0934f2791d40695b3f474249cf7c0505625c
zhongyw <zw198307@163.com>
2016-11-11 11:52:24 +0800

    npm git support

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 
 gulp.task("default", function () {
-  return gulp.src("src/index.js")
+  return gulp.src("lib/index.js")
     .pipe(babel())
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("bin"));
 });
